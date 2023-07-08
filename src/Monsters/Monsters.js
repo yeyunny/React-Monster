20063d1b879f251cd57c7161cf1dd2ca16d19170
yeyun <hallo.yunye@gmail.com>
2023-07-08 20:55:42 +0900

    [ADD] 파일 구성

--- src/Monsters/Monsters.js
+++ src/Monsters/Monsters.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import './Monsters.scss';
+import React, { useEffect, useState } from "react";
+import "./Monsters.scss";
 
 function Monsters() {
+  const [monsterList, setMonsterList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((result) => result.json())
+      .then((data) => setMonsterList(data));
+  }, []);
+  console.log(monsterList);
   return (
     <div className="monsters">
       <h1 className="title">Monsters</h1>
